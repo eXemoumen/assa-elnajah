32a254fa2b8de4683e95c6a2c1847356a90be479
Moumen.exe <ourmoumen@gmail.com>
2023-12-27 11:05:10 +0100

    i added the location methode

---
+++ assa najah/js/main.js
@@ -0,0 +1,6 @@
+var loc = document.getElementById("loc");
+loc.addEventListener("click", () => {
+  window.open(
+    "https://www.google.com/maps/place/School+Assas+El+Nadjah/@36.7341951,3.0685549,17z/data=!3m1!4b1!4m6!3m5!1s0x128fad4b3a583be1:0x4a0bd8302c2e4ede!8m2!3d36.7341951!4d3.06598!16s%2Fg%2F11sb75x8_x?entry=ttu"
+  );
+});
